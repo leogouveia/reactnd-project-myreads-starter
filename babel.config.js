50ba904785f8d6527c7d1507c84cd63158c883cf
Leonardo de Albuquerque Gouveia <leogouv@gmail.com>
2018-10-27 17:41:43 -0300

    Fix error in babel config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,7 @@
-const babelrc = require('.babelrc');
+const fs = require('fs');
+const babelrc = JSON.parse(fs.readFileSync('./.babelrc'));
+
 module.exports = {
-  presets: babelrc.presets,
-  plugins: babelrc.plugins
+  "presets": babelrc.presets,
+  "plugins": babelrc.plugins
 }
